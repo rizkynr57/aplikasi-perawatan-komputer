f3ee8dfd9b18f5502b3fb69d745ec2b7a3a9f648
rizkyn57 <rizky.nurahman48@gmail.com>
2022-02-04 04:47:18 +0500

    projek

---
+++ lib/main_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            leading: Icon(Icons.home_max),
+            title: Text('Perawatan Komputer dan Laptop')),
+        body: ListView(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 25),
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(50),
+                    height: 250,
+                    width: 350,
+                    decoration: BoxDecoration(
+                      color: Colors.amberAccent,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/gambar.jpg'),
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 25),
+                    height: 250,
+                    width: 350,
+                    decoration: BoxDecoration(
+                      color: Colors.amberAccent,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.red),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
